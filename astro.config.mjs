b099908423f2cb532ae1d3f5c65fcb8bfe505dee
Ramil Shagiev <ram1337@MacBook-Pro-ram1337.local>
2025-12-30 13:21:39 +0300

    ikev2 tutor, astro sitemap, syntax highlighter, copy button

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import mdx from "@astrojs/mdx";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 
   site: "https://ram1337.ru",
   vite: {
